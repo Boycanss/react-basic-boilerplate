24bf833f5ae7e1ec3709622192ec177f89eeeb54
GarlicCS <boy.simanjuntak49@gmail.com>
2019-12-12 01:37:25 +0700

    template

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import Home from './Pages/Home/Home';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Route
+          exact={true}
+          path="/"
+          component={Home}
+        />
+      </Router>
+    )
+  }
+}
